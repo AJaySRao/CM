d6d632b94a2a2411bbda6d5e3024bd5a2932be59
changeAJay <ajajay008@gmail.com>
2022-02-09 07:51:06 +0530

    1. Updated preparations to ingredients check, which checks with resources ingredients whether the user entered drink can be made 2. When a user enters a drink name, now directly checks ingredients and if that turns out true then processes the coins 3. Creates a method coin process 4. Resolved the transaction conflict over coins process and ingredients deduction when user enters insufficient money 5. Added doc-types to avoid confusion now 6.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ resources = {'Water': 300,
              'Money': 0 }
 
 Menu_items = {'espresso': {'Water': 50,
-                           'Milk': 0,
+                           #'Milk': 0,
                            'Coffee': 18,
                            'Money': 3.5},
               'latte': {'Water': 200,
@@ -26,31 +26,43 @@ total_money = 0
 
 
 def report():
-
+    """Report of the resources in the Machine"""
     print(f"Water:{resources['Water']}ml "
           f"\nMilk:{resources['Milk']}ml "
           f"\nCoffee:{resources['Coffee']}mg "
           f"\nMoney:{resources['Money']}$")
 
 
+def coin_process():
+    """Process the respective value of each entered coin"""
+    global total_money
+    print("Please insert coins")
+    for coin in coins:
+        received = int(input(f"how many {coin}?:"))
+        total_money += coins[coin]*received
+
+
 def transaction(total_money):
+    """Validates the transaction made, and returns the entered extra amount back to the user"""
     if total_money >= Menu_items[choice]['Money']:
         resources['Money'] += Menu_items[choice]['Money']
         total_money -= Menu_items[choice]['Money']
         amt = '{:.2f}'.format(total_money)
         print(f"Here's the change:{amt}$ \n")
+        return True
     else:
-        print(f"Sorry that's not enough money \n")
+        return False
 
 
-def preparation(choice):
+def ingredient_check(choice):
+    """Checks with resources ingredients, whether the user entered drink can be made"""
     for item in resources:
         if resources[item] >= Menu_items[choice][item]:
-            resources[item] -= Menu_items[choice][item]
+            if transaction(total_money):
+                resources[item] -= Menu_items[choice][item]
             return True
         else:
             return False
-
 
 machine = True
 while machine:
@@ -63,19 +75,15 @@ while machine:
         machine = False
     else:
         if choice in Menu_items:
-            print("Please insert coins")
-
-            for coin in coins:
-                received = int(input(f"how many {coin}?:"))
-                total_money += coins[coin]*received
-
-            if preparation(choice):
-                print(f"Here's your {choice}, Please enjoy!")
+            if ingredient_check(choice):
+                coin_process()
+                if transaction(total_money):
+                    print(f"Here's your {choice}, Please enjoy!")
+                    total_money = 0
+                else:
+                    print(f"Sorry that's not enough money \n")
             else:
                 print(f"Sorry there are no enough resources!")
-
-            transaction(total_money)
-            total_money = 0
         else:
             print("Please only choose the available drinks ! \n")
 
